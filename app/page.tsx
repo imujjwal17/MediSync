7c655280494e93cb1090a99bcc512e2a8e252936
Kunwar Ujjwal Singh <kujjwal21378@gmail.com>
2024-08-22 17:11:31 +0530

    environment setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return(
+    <div className="flex h-screen max-h-screen">
+      <h1 className="text-3xl underline text-white">Home</h1>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
